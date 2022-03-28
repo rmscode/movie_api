364ce5e311fd68d44a7dd358c2648a0e343ad0f2
rmscode <ricky.saull@gmail.com>
2022-03-28 16:36:40 -0400

    add documentation.html page
    if parsing request.url does not contain "documentation", return index.html.

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const http = require('http'),
   url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
+    let addr = request.url,
+      q = url.parse(addr, true),
+      filepath = '';
+      
+    if (q.pathname.includes('documentation')) {
+      filePath = (__dirname + '/documentation.html');
+    } else {
+      filePath = 'index.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.write(data);
+      response.end('Hello Node!\n');
+      
+    });
+
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
