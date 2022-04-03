6e199598bc7876d914de5d9585fbfd7ba5f43096
rmscode <ricky.saull@gmail.com>
2022-04-03 14:02:11 -0400

    initialize server on port 8080 and fix small syntax error

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express'),
   morgan = require('morgan'),
   fs = require('fs'),
   path = require('path');
+const res = require('express/lib/response');
 
-const app = express;
+const app = express();
 // Create a write stream in append mode . . 'log.txt' is created in root dir
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
 app.use(morgan('common'));
 
+app.listen(8080, () => {
+  console.log('The Jackie Chan Movie app is listening on port 8080.');
+});
