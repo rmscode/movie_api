63387f3b9a85b2a20cd927d2b14b317dd11b9e63
rmscode <ricky.saull@gmail.com>
2022-04-03 13:55:02 -0400

    add method to append Morgan logs to file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Importing modules
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path');
 
 const app = express;
+// Create a write stream in append mode . . 'log.txt' is created in root dir
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
 app.use(morgan('common'));
 
