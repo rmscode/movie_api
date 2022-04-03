65a795a91c956a1c982ad6b98fc643e048c9b9d6
rmscode <ricky.saull@gmail.com>
2022-04-03 14:18:43 -0400

    add error handling and cleanup code base

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const express = require('express'),
   morgan = require('morgan'),
   fs = require('fs'),
   path = require('path');
-const res = require('express/lib/response');
+  res = require('express/lib/response');
+  app = express();
+  // Create a write stream in append mode . . 'log.txt' is created in root dir
+  accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-const app = express();
-// Create a write stream in append mode . . 'log.txt' is created in root dir
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+// Morgan will log requests
+  app.use(morgan('common'));
 
-app.use(morgan('common'));
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!!!');
+});
 
 app.listen(8080, () => {
   console.log('The Jackie Chan Movie app is listening on port 8080.');
