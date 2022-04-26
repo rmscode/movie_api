c5cc9590f09dd4c699433b60daff220b04a88630
rmscode <ricky.saull@gmail.com>
2022-04-26 12:34:28 -0400

    fixed typo

--- models.js
+++ models.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
     },
     Director: {
         Name: String,
-        Bio: stringify,
+        Bio: String,
     },
     Actors: [String],
     ImagePath: String,
