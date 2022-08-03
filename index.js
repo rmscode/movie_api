8b0f38a6e14d3e1f537800fd4f05659b9dacaac4
rmscode <ricky.saull@gmail.com>
2022-08-03 15:32:28 -0400

    temporarily removing auth middleware
    testing with my-flix client

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ app.delete(
 // Get all movies
 app.get(
     '/movies',
-    passport.authenticate('jwt', { session: false }),
+    // passport.authenticate('jwt', { session: false }),
     (req, res) => {
         Movies.find()
             .then((movies) => {
